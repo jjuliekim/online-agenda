5f152828a8e5b27b4c7dcaf2394fe28307b33db4
jjuliekim <118621382+jjuliekim@users.noreply.github.com>
2023-07-16 21:17:29 +0900

    clearing data

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 // update today's date
-let dateLabel = document.getElementById("date-label");
+const dateLabel = document.getElementById("date-label");
 let date = new Date();
-const day= date.getDay()
-let weekday= ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const day = date.getDay()
+const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 dateLabel.innerText = "Today is " + weekday[day] + " (" + (date.getMonth() + 1) + "/" + date.getDate() + ")";
 
+// clear agenda
+function clearClass(elementClass) {
+    document.getElementsByClassName(elementClass).innerText = "";
+}
+
+/*
+clear only that day's data
+document.getElementsByClassName("clear").onclick = function() {
+    clearClass("clear");
+    clearClass("event-list");
+    clearClass("task-list");
+}*/
