28c55a132bbfc590840330bd93206f107565e3f0
jjuliekim <118621382+jjuliekim@users.noreply.github.com>
2023-07-18 21:54:08 +0900

    valid new event prompt popup

--- script.js
+++ script.js
@@ -5,15 +5,33 @@ const day = date.getDay()
 const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 dateLabel.innerText = "Today is " + weekday[day] + " (" + (date.getMonth() + 1) + "/" + date.getDate() + ")";
 
-// clear agenda
-function clearClass(elementClass) {
-    document.getElementsByClassName(elementClass).innerText = "";
-}
 
-/*
-clear only that day's data
-document.getElementsByClassName("clear").onclick = function() {
-    clearClass("clear");
-    clearClass("event-list");
-    clearClass("task-list");
-}*/
+// new event
+function newEvent() {
+    let title = prompt("Event Title:");
+    while (title.trim() === "") {
+        title = prompt("Invalid event title! \nEnter valid title:");
+    }
+    let description = prompt("Event Description:");
+    while (description.trim() === "") {
+        description = prompt("Invalid event description!\nEnter valid description:");
+    }
+    let day = prompt("Day of Event:",
+        "Write as: Sunday, Monday, Tuesday, ...");
+    while (!weekday.includes(day.trim())) {
+        day = prompt("Invalid day!\nEnter valid a day of the week:");
+    }
+    let time = prompt("Time of Event:", "Write as: hh:mm AM/PM");
+    if (time == null) return;
+    const regex = /^([1-9]|[01][0-2]):(\d|[0-5]\d) (AM|PM)$/i;
+    while (!regex.exec(time)) {
+        time = prompt("Invalid event time! \nEnter valid time:", "Write as: hh:mm AM/PM");
+    }
+    const result = regex.exec(time);
+    const hour = result[1];
+    const minute = result[2];
+    const isAM = result[3].toLowerCase() === "am";
+
+    // add the new event to the event list
+
+}
